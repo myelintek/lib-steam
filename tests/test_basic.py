a4a670110a2f73a080f635208255ad7a3737fb49
simonschuang <simon.s.chuang@gmail.com>
2019-08-05 17:45:55 +0800

    Update and rename test_mlperf.py to test_basic.py
    Add cifar10_delete

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,6 +4,7 @@ import os
 import time
 from os import system
 
+
 def test_login():
     child = pexpect.spawn('mlsteam login --address 140.96.29.151 --username superuser')
     child.expect ('password:')
@@ -13,17 +14,21 @@ def test_login():
     exp=re.findall(b"Login success", out)
     assert exp==[b'Login success']
 
+
 def test_data_mb():
     ret=system("mlsteam data mb test")
     assert ret==0
+
 
 def test_data_cp():
     ret=system("mlsteam data cp /workspace/tests/fake_dataset/file1 bk/test")
     assert ret==0
 
+
 def test_data_ls():
     ret=system("mlsteam data ls bk/test")
     assert ret==0
+
 
 def test_data_rm():
     ret=system("mlsteam data rm bk/test/file1")
@@ -39,20 +44,25 @@ def test_generate_cifar10():
     system("mlsteam data cp -r /workspace/cifar10/* bk/cifar10")
     system("mlsteam data ls bk/cifar10")
 
+
 def test_project_create():
     system("mlsteam project create test_project cifar10")
+
 
 def test_job_submit():
     ret=system("PROJECT=test_project mlsteam job submit training --job-name job1 --package-path /workspace/example/cifar10_estimator -- python2 cifar10_main.py --data-dir /dataset --job-dir /jobs --train-steps=100")
     assert ret==0
 
+
 def test_job_list():
     ret=system("mlsteam job list")
     assert ret==0
 
+
 def test_project_list():
     ret=system("mlsteam project list")
     assert ret==0
+
 
 def test_job_status():
     job_status=os.popen("mlsteam job list | awk '/job1/ {print $8}'").read()
@@ -78,8 +88,13 @@ def test_job_delete():
         ret=system("mlsteam job delete --job-id "+job_id)
         assert ret==0
 
+
 def test_project_delete():
     proj_id=os.popen("mlsteam project list | awk '/test_project/ {print $2}'").read()
     print(proj_id)
     ret=system("mlsteam project delete --id "+proj_id)
     assert ret==0
+
+
+def test_cifar10_delete():
+    system("mlsteam data rb cifar10")
