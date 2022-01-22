fdfb0d8bb841fd2f1b44b3c9373a05cea49f23ad
SimonChuang <simon.s.chuang@gmail.com>
2022-01-22 08:45:27 +0000

    Bump to version 0.4.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os.path
 from setuptools import find_packages, setup
 # from mlsteam.version import __version__
-__version__="0.4.5"
+__version__="0.4.6"
 
 with open(
     os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"),
