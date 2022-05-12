c70eb797cde93a6e65080ba19fca6e79129d3621
mm25712423 <mm25712423@gmail.com>
2022-05-12 10:15:19 +0800

    fix bug and version

--- mlsteam/stparams.py
+++ mlsteam/stparams.py
@@ -3,29 +3,42 @@ import click
 import logging
 import yaml
 
+filename = 'mlsteam.yml'
+HOME = os.getenv('HOME', '/')
 
-## get value from HOME/lab/mlsteam.yml
+# PWD/mlsteam.yml
+# HOME/mlsteam.yml
+# HOME/lab/mlsteam.yml
+
+
 def get_value(key, default=None):
-    _dir = os.getenv('HOME', None)
-    if not _dir:
-        logging.warning("use default value for {}, HOME environment variable undefined.".format(key))
+    dirs = [os.getcwd(), HOME, os.path.join(HOME, 'lab')]
+    param_file = ""
+    for _dir in dirs:
+        param_file = os.path.join(_dir, filename)
+        if os.path.exists(param_file):
+            return
+
+    if not param_file:
+        logging.warning(
+            "use default value for {}, mlsteam.yml not found.".format(key))
         return default
-    param_file = os.path.join(_dir, "lab", "mlsteam.yml")
-    if not os.path.exists(param_file):
-        logging.warning("use default value for {}, mlsteam.yml not found.".format(key))
-        return default
+
     params = {}
     with open(param_file, encoding='utf-8', mode='r') as f:
         params = yaml.safe_load(f)
     if "params" not in params:
-        logging.warning("use default value for {}, undefined variable.".format(key))
+        logging.warning(
+            "use default value for {}, undefined variable.".format(key))
         return default
     for k, v in params["params"].iteritems():
         if key == k:
             if isinstance(params["params"][key], list):
-                click.echo("hyperparameter - {}: {}".format(key, params["params"][key][0]))
+                click.echo("hyperparameter - {}: {}".format(key,
+                           params["params"][key][0]))
                 return params["params"][key][0]
-            click.echo("hyperparameter - {}: {}".format(key, params["params"][key]))
+            click.echo(
+                "hyperparameter - {}: {}".format(key, params["params"][key]))
             return params["params"][key]
     click.echo("hyperparameter - {}: {}(default)".format(key, default))
     return default
